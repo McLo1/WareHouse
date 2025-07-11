cecb0443e1b659c5cd5f178421d3cd7aeeb86081
McLo1 <marcelo.conceicao1718@gmail.com>
2025-07-11 19:14:17 -0300

    Adicionado propriedade ID na classe, no construtor e nos Getters e Setters

--- src/main/java/com/example/Backend/dto/FerramentaDTO.java
+++ src/main/java/com/example/Backend/dto/FerramentaDTO.java
@@ -5,10 +5,27 @@ import com.example.Backend.Enuns.FerramentaStatus;
 
 public class FerramentaDTO {
 
+    private Long id;
     private String nome;
     private String marca;
     private String tipo;
     private FerramentaStatus ferramentaStatus;
+
+    public FerramentaDTO(Long id, String nome, String marca, String tipo, FerramentaStatus ferramentaStatus) {
+        this.id = id;
+        this.nome = nome;
+        this.marca = marca;
+        this.tipo = tipo;
+        this.ferramentaStatus = ferramentaStatus;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getNome() {
         return nome;
