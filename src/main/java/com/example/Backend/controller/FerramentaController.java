ec9b7225d2b0f92259169e60cbd1f26173550cdc
McLo1 <marcelo.conceicao1718@gmail.com>
2025-07-11 20:17:29 -0300

    Criado os metodos salvar, listar, atualizar e deletar do controller

--- src/main/java/com/example/Backend/controller/FerramentaController.java
+++ src/main/java/com/example/Backend/controller/FerramentaController.java
@@ -1,8 +1,14 @@
 package com.example.Backend.controller;
 
+import com.example.Backend.dto.FerramentaDTO;
+import com.example.Backend.dto.FerramentaResponseDTO;
 import com.example.Backend.service.FerramentaService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/ferramenta")
@@ -15,4 +21,24 @@ public class FerramentaController {
     }
 
 
+    @GetMapping
+    public List<FerramentaResponseDTO> listarTodos() {
+        return ferramentaService.listarTodos();
+    }
+
+    @PostMapping
+    public ResponseEntity<FerramentaResponseDTO> salvar(FerramentaDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(ferramentaService.salvar(dto));
+    }
+
+    @PutMapping
+    public ResponseEntity<FerramentaResponseDTO> atualizar(@RequestBody FerramentaDTO dto) {
+        return ResponseEntity.status(HttpStatus.OK).body(ferramentaService.atualizar(dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<FerramentaResponseDTO> deletar(@PathVariable Long id) {
+        ferramentaService.excluir(id);
+        return ResponseEntity.noContent().build();
+    }
 }
