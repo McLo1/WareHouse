3ef814b2a1acdac5b93da34c14c36e75cb0f341a
McLo1 <marcelo.conceicao1718@gmail.com>
2025-07-11 19:36:14 -0300

    Criado metodos atualizar e excluir

--- src/main/java/com/example/Backend/service/FerramentaService.java
+++ src/main/java/com/example/Backend/service/FerramentaService.java
@@ -4,6 +4,7 @@ import com.example.Backend.dto.FerramentaDTO;
 import com.example.Backend.dto.FerramentaResponseDTO;
 import com.example.Backend.model.Ferramenta;
 import com.example.Backend.repository.FerrametaRepository;
+import com.fasterxml.jackson.core.PrettyPrinter;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -55,5 +56,38 @@ public class FerramentaService {
         );
         return ferramentaResponseDTO;
     }
+
+    public FerramentaResponseDTO atualizar(FerramentaDTO ferramentaDTO) {
+        Ferramenta ferramenta = ferrametaRepository.findById(ferramentaDTO.getId()).map(f -> {
+            f.setId(null);
+            f.setNome(ferramentaDTO.getNome());
+            f.setMarca(ferramentaDTO.getMarca());
+            f.setTipo(ferramentaDTO.getTipo());
+            f.setFerramentaStatus(ferramentaDTO.getFerramentaStatus());
+
+            return f;
+        })
+                .orElseThrow(() -> new RuntimeException("Ferramenta não encontrada"));
+
+        ferrametaRepository.save(ferramenta);
+
+        FerramentaResponseDTO ferramentaResponseDTO = new FerramentaResponseDTO(
+                ferramenta.getId(),
+                ferramenta.getNome(),
+                ferramenta.getMarca(),
+                ferramenta.getTipo(),
+                ferramenta.getFerramentaStatus()
+        );
+        return ferramentaResponseDTO;
+    }
+
+    public void excluir(Long id) {
+        Ferramenta ferramenta = ferrametaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Ferramenta não encontrada"));
+
+        ferrametaRepository.deleteById(id);
+    }
+
+
 
 }
