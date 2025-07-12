230af5292b14384f0602aeee4e760b03648f5d42
McLo1 <marcelo.conceicao1718@gmail.com>
2025-07-12 11:34:33 -0300

    Removido FindByNome, mantendo FindByID na aplicação

--- src/main/java/com/example/Backend/repository/FerrametaRepository.java
+++ src/main/java/com/example/Backend/repository/FerrametaRepository.java
@@ -4,6 +4,7 @@ import com.example.Backend.model.Ferramenta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface FerrametaRepository extends JpaRepository<Ferramenta, Long> {
 }
